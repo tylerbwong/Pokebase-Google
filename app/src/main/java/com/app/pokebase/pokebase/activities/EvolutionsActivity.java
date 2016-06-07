6ca967cf181b2710320e2ae655108f46562091bd
brittanyberlanga <brittany.berlanga@gmail.com>
2016-06-06 18:18:20 -0700

    added profile and evolutions activities

---
+++ app/src/main/java/com/app/pokebase/pokebase/activities/EvolutionsActivity.java
@@ -0,0 +1,38 @@
+package com.app.pokebase.pokebase.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.app.pokebase.pokebase.R;
+
+/**
+ * Created by brittanyberlanga on 6/6/16.
+ */
+public class EvolutionsActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evolutions);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/app/pokebase/pokebase/activities/ProfileActivity.java
@@ -0,0 +1,198 @@
+package com.app.pokebase.pokebase.activities;
+
+import android.content.Intent;
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.app.pokebase.pokebase.R;
+
+/**
+ * Created by brittanyberlanga on 6/4/16.
+ */
+public class ProfileActivity extends AppCompatActivity {
+    private static final double FT_PER_DM = 0.32808399;
+    private static final double LB_PER_HG = 0.22046226218;
+    private static final int KG_PER_HG = 10;
+    private static final int IN_PER_FT = 12;
+    private static final int DM_PER_M = 10;
+    private static final int PROFILE_IMG_ELEVATION = 40;
+    private Toolbar mToolbar;
+    private ImageView mProfileImg;
+    private TextView mIdView;
+    private TextView mNameView;
+    private TextView mTypeOneView;
+    private TextView mTypeTwoView;
+    private TextView mSpeciesView;
+    private TextView mRegionView;
+    private TextView mHeightView;
+    private TextView mWeightView;
+    private TextView mExpView;
+    private ListView mMovesList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mProfileImg = (ImageView) findViewById(R.id.profile_image);
+        mProfileImg.setClipToOutline(true);
+        mProfileImg.setElevation(PROFILE_IMG_ELEVATION);
+        mIdView = (TextView) findViewById(R.id.id);
+        mNameView = (TextView) findViewById(R.id.name);
+        mTypeOneView = (TextView) findViewById(R.id.type_one);
+        mTypeTwoView = (TextView) findViewById(R.id.type_two);
+        mSpeciesView = (TextView) findViewById(R.id.species);
+        mRegionView = (TextView) findViewById(R.id.region);
+        mHeightView = (TextView) findViewById(R.id.height);
+        mWeightView = (TextView) findViewById(R.id.weight);
+        mExpView = (TextView) findViewById(R.id.exp);
+        mMovesList = (ListView) findViewById(R.id.moves_list);
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+        String[] moves = new String[10];
+        moves[0] = "blah blah";
+        moves[1] = "hello";
+        moves[2] = "eevee";
+        moves[3] = "shaymin";
+        moves[4] = "blah blah";
+        moves[5] = "blah blah";
+        moves[6] = "hello";
+        moves[7] = "eevee";
+        moves[8] = "shaymin";
+        moves[9] = "blah blah";
+        mMovesList.setAdapter(new TextViewAdapter(moves));
+        setHeightViewText(3);
+        setWeightViewText(65);
+
+        actionBar.setTitle("Eevee");
+
+    }
+
+    private void setHeightViewText(int decimeters)
+    {
+        int feet = (int) Math.floor(decimeters * FT_PER_DM);
+        int inches = (int) Math.round((decimeters * FT_PER_DM - feet) * IN_PER_FT);
+        if (inches == IN_PER_FT)
+        {
+            feet++;
+            inches = 0;
+        }
+        double millimeters = (double) decimeters / DM_PER_M;
+        String heightText = feet + "'" + inches + "'' (" + String.format("%.2f", millimeters)
+                + " m)";
+        mHeightView.setText(heightText);
+    }
+
+    private void setWeightViewText(int hectograms)
+    {
+        double pounds = hectograms * LB_PER_HG;
+        double kilograms = (double) hectograms / KG_PER_HG;
+        String weightText = String.format("%.1f", pounds) + " lbs (" +
+                String.format("%.1f", kilograms) + " kg)";
+        mWeightView.setText(weightText);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    public void showEvolutions(View view)
+    {
+        Intent evolutionsIntent = new Intent(this, EvolutionsActivity.class);
+        startActivity(evolutionsIntent);
+    }
+
+    public final class TextViewAdapter implements ListAdapter {
+        private String[] items;
+        public TextViewAdapter(String[] items)
+        {
+            super();
+            this.items = items;
+        }
+
+        @Override
+        public boolean isEnabled(int position) {
+            return false;
+        }
+
+        @Override
+        public boolean areAllItemsEnabled() {
+            return false;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return items.length == 0;
+        }
+
+        @Override
+        public void registerDataSetObserver(DataSetObserver observer) {}
+
+        @Override
+        public void unregisterDataSetObserver(DataSetObserver observer) {}
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView textView = new TextView(ProfileActivity.this);
+            textView.setTextSize(16);
+            textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            textView.setLayoutParams(lp);
+            textView.setText(items[position]);
+            return textView;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return 1;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 1;
+        }
+    }
+}
